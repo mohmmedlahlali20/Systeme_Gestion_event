fac029d0b49db71ee8995cac8f21372f2b158c17
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-25 16:57:39 +0100

    Create: Create schema for users

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  firstName: string;
+  @Prop({ required: true })
+  lastName: string;
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+
+export const userSchema = SchemaFactory.createForClass(User)
