0ba10c3495939e639dc9816c8a2fae9717c8094a
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-26 09:25:04 +0100

    Add: Add connection between projet and dataBase

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.Mongo_URI)
+
+    AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
