a11531ee3da30ae21e17147da4a27d35d8bb4c47
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-26 09:57:24 +0100

    Create: create EndPoints Register new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,28 +7,12 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('/register')
+  async register(@Body() userDto: CreateAuthDto) {
+    const newUser = await this.authService.register(userDto);
+    return {
+      message: 'User created successfully',
+      user: newUser,
+    };
   }
 }
