7874fe65f2de142a2e1eeba51e2851bb1991c4e4
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-26 17:14:59 +0100

    Import: Import event Model in  App Model

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from "@nestjs/mongoose";
+import { EventModule } from './event/event.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.Mongo_URI || 'mongodb://localhost:27017/systeme_gestion_event'),
     AuthModule, 
-    UsersModule],
+    UsersModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
