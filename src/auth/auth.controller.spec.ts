d321f4b9b29ef84a0a0640dac2b183fd2ba4de79
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-26 17:05:40 +0100

    Create: create test for register function

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { CreateAuthDto, Role } from './dto/create-auth.dto';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
+    const mockAuthService = {
+      register: jest.fn(),
+      login: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should call AuthService.register with the correct data and return the expected result', async () => {
+      const dto: CreateAuthDto = {
+        email: 'luffy@gmail.com',
+        password: 'luffy_luffy',
+        firstName: 'luffy',
+        lastName: 'monkey D',
+        role: Role.User,
+      };
+      const expectedResult = { userId: '1', email: 'luffy@gmail.com' };
+
+      jest.spyOn(authService, 'register').mockResolvedValue(expectedResult);
+
+      const result = await authController.register(dto);
+
+      expect(authService.register).toHaveBeenCalledWith(dto);
+      expect(result).toEqual(expectedResult); 
+    });
   });
 });
