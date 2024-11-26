f6b57a65edbdfb27b4c59b477afa8d2e7b547624
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-26 20:46:24 +0100

    Create: Create Validation Create Event (DTO)

--- src/event/dto/create-event.dto.ts
+++ src/event/dto/create-event.dto.ts
@@ -1,1 +1,27 @@
-export class CreateEventDto {}
+import { IsArray, IsDate, IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+
+
+
+export class CreateEventDto {
+    @IsNotEmpty()
+    @IsString()
+    Title: string;
+
+
+    @IsNotEmpty()
+    @IsString()
+    Description: string;
+
+
+    @IsNotEmpty()
+    @IsMongoId()
+    @IsArray()
+    members: string[];
+
+
+    @IsDate()
+    @IsNotEmpty()
+    Date: Date
+
+
+}
