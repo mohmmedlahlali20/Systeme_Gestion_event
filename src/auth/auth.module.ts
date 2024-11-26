c30e5d2a0e55216c8d5161999ea44228f1cc8c40
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-26 17:07:53 +0100

    Fix: Fix JWT in model

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,15 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, User } from 'src/users/schemas/users.schema';
+import { JwtModule } from '@nestjs/jwt';
+
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{name: User.name, schema: UserSchema}])
+    MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'sucretKEyO',
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
