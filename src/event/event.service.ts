71c3975d1ccb4115f2b4a1721bcd49247b0afb7b
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-26 21:54:50 +0100

    feat: create features for adding new event

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { eventDocument, Event } from './schemas/event.schema';
 
 @Injectable()
 export class EventService {
-  create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+
+  constructor(
+    @InjectModel(Event.name) private readonly eventModel: Model<eventDocument>,
+
+  ) { }
+
+
+
+
+
+  async create(createEventDto: CreateEventDto): Promise<any>  {
+    const { Title, Description, Members } = createEventDto
+    const createEvent = new this.eventModel({
+      Title,
+      Description,
+      Members,
+    })
+    const newEvent = await createEvent.save();
+    return {
+      message: "event created success",
+      newEvent
+    }
   }
+
+
+  
+  
 
   findAll() {
     return `This action returns all event`;
