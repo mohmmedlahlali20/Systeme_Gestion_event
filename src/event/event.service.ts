672d4f91ebdff302764f8103aa50fdc654cddfc9
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-27 11:41:32 +0100

    feat: add feat for event (update, add member , remove memeber)

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId } from 'mongoose';
+import { Model, ObjectId, Types } from 'mongoose';
 import { eventDocument, Event } from './schemas/event.schema';
 
 @Injectable()
@@ -17,7 +17,7 @@ export class EventService {
 
 
 
-  async create(createEventDto: CreateEventDto): Promise<any>  {
+  async create(createEventDto: CreateEventDto): Promise<any> {
     const { Title, Description, Members } = createEventDto
     const createEvent = new this.eventModel({
       Title,
@@ -32,39 +32,73 @@ export class EventService {
   }
 
 
+  async updateEvent(eventId: string, updateEventDto: UpdateEventDto): Promise<any> {
+    const event = await this.eventModel.findByIdAndUpdate(eventId, updateEventDto, {
+      new: true,
+    });
+    return event;
+  }
+
+
   async addMember(eventId: ObjectId, userId: ObjectId): Promise<any> {
     const event = await this.eventModel.findById(eventId);
-  
+
     if (!event) {
-      throw new Error('Event not found'); 
+      throw new Error('Event not found');
     }
-  
+
     if (event.members.includes(userId)) {
       return {
         message: 'User is already a member of the event',
       };
     }
-      event.members.push(userId);
-      await event.save();
-  
+    event.members.push(userId);
+    await event.save();
+
     return {
       message: 'Member added successfully',
       event,
     };
   }
+
+
+  async removeMemberFromEvent(userId: string, eventId: string): Promise<any> {
+    const event = await this.eventModel.findById(eventId);
+  
+    if (!event) {
+      return { message: `No event found with id: ${eventId}` };
+    }
+  
+    const userObjectId = new Types.ObjectId(userId);
+  
+    event.members = event.members.filter(member => member.toString() !== userObjectId.toString());
+  
+    await event.save();
+  
+    return { message: `User with id: ${userId} removed from event.` };
+  }
+  
   
 
-  findAll() {
-    return `This action returns all event`;
+
+  async findAll(): Promise<Event[]> {
+
+    const evnets = await this.eventModel.find().populate('members')
+    if (evnets.length === 0) {
+      console.log("No events found");
+      return [];
+    }
+    return evnets
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  async findOne(eventId: string): Promise<Event> {
+    const eventById = await this.eventModel.findById(eventId).populate('members');
+    if (!eventById) {
+      throw new Error(`No event found with id: ${eventId}`);
+    }
+    return eventById;
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
-  }
 
   async remove(eventId: ObjectId): Promise<any> {
     const deletedEvent = await this.eventModel.findByIdAndDelete(eventId);
