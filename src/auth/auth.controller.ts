aa5f78eba5a7ddb1ab829713490b6d8413776365
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-29 09:49:01 +0100

    feat: enhance auth controller logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+
 
 @Controller('auth')
+
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
